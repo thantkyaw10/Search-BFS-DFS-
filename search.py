2f63ee18bafdb20fef570c55257e1aec2c9e4857
thantkyaw10 <46037388+thantkyaw10@users.noreply.github.com>
2019-09-17 17:33:13 -0400

    Deleted empty dfs function

--- search.py
+++ search.py
@@ -107,21 +107,6 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
     #util.raiseNotDefined()
     explored = [] #list for states already explored
     frontier = util.Stack() #using stack data structure
